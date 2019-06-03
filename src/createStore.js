1552b4342f5cb436db67c3845ab9b37330fecff1
jwy5140 <jwy5140@gmail.com>
2019-06-03 00:17:00 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,22 @@
 export default function createStore(reducer) {
-  // add your code here
+  
+  let state;
+
+  const getState = () => {
+    return state;
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    render();
+  }
+
+  dispatch({type: '@@INIT'})
+
+  return {getState, dispatch}
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+
